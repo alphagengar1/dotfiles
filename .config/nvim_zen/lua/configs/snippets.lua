d0dbc1eac5499ddfe9fbdc91f1f95b9b893ddc35
Swaraj Adhikari <swaraj2065@gmail.com>
2026-02-11 13:31:17 -0800

    Sync nvim_zen with nvim

--- .config/nvim_zen/lua/configs/snippets.lua
+++
@@ -1,5 +0,0 @@
-local luasnip = require("luasnip")
-
--- Explicitly require the loaders to avoid indexing nil
-require("luasnip.loaders.from_lua").load({ paths = vim.fn.stdpath("config") .. "/lua_snippets/my_snippets" })
-require("luasnip.loaders.from_vscode").lazy_load()
