59bc0b580de0491aeb747561cae0f3de29d2f438
Swaraj Adhikari <swaraj2065@gmail.com>
2026-06-08 19:51:17 -0700

    Show directory listing after cd

--- .config/zsh/navigation.zsh
+++ .config/zsh/navigation.zsh
@@ -1,13 +1,12 @@
-_silent_ls_after_chpwd() {
-  {
-    if command -v eza >/dev/null 2>&1; then
-      eza --icons=always . >/dev/null 2>&1
-    else
-      command ls . >/dev/null 2>&1
-    fi
-  } </dev/null &|
+_ls_after_chpwd() {
+  if command -v eza >/dev/null 2>&1; then
+    eza --icons=always .
+  else
+    command ls .
+  fi
 }
 
 autoload -Uz add-zsh-hook
 add-zsh-hook -d chpwd _silent_ls_after_chpwd 2>/dev/null
-add-zsh-hook chpwd _silent_ls_after_chpwd
+add-zsh-hook -d chpwd _ls_after_chpwd 2>/dev/null
+add-zsh-hook chpwd _ls_after_chpwd
