eaee592bf8f9aefde55041e0371f294db328e4fd
Swaraj Adhikari <swaraj2065@gmail.com>
2026-06-08 18:45:19 -0700

    Keep transient prompt enabled by default

--- .config/zsh/transient.zsh
+++ .config/zsh/transient.zsh
@@ -1,8 +1,9 @@
 # Oh My Posh implements transient prompts by redrawing the previous prompt on
-# Enter. That compression is neat, but the redraw can flicker in some terminals.
+# Enter. Keep compression enabled by default, but allow a local opt-out if a
+# terminal makes the redraw too distracting.
 #
-# Set ZSH_TRANSIENT_PROMPT=1 before sourcing .zshrc to keep compression enabled.
-: ${ZSH_TRANSIENT_PROMPT:=0}
+# Set ZSH_TRANSIENT_PROMPT=0 in ~/.zshrc.local to disable compression entirely.
+: ${ZSH_TRANSIENT_PROMPT:=1}
 
 if [[ "$ZSH_TRANSIENT_PROMPT" != 1 && ${widgets[zle-line-init]-} == user:_omp_zle-line-init ]]; then
   zle -D zle-line-init
